3b41bfb0843b30e996a8c294ada8215338810b7d
Anmol Thakur <anmol257777@gmail.com>
2025-04-10 12:29:22 +0530

    some components created

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,1 +1,18 @@
-rnfes
+import { Image,StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { images } from '@/constants/images'
+import { ScrollView } from 'react-native-reanimated/lib/typescript/Animated'
+
+const index = () => {
+  return (
+   <View className='flex-1 bg-primary'>
+    <Image source={images.bg} className="absolute w-full z-0"/>
+
+    <ScrollView
+   </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
